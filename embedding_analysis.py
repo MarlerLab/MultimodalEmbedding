be9f3b92a74251314649f6aa7ee101d1d7b4d736
yjang43 <yjang43@wisc.edu>
2022-09-18 02:50:27 +0900

    Embedding analysis

---
+++ embedding_analysis.py
@@ -0,0 +1,106 @@
+# import 
+import os
+import yaml
+import torch
+import torch.nn as nn
+import matplotlib.pyplot as plt
+
+from attrdict import AttrDict
+from sklearn.manifold import TSNE
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+from models import MMEncoder
+from dataset import MMDataset
+
+
+
+# define function where you put modality and get print out the plot
+def analyze_embedding(modality, samples, reduction_model):
+    num_labels = len(samples)
+    modality_samples_list = []
+    
+    for label in range(num_labels):
+        modality_samples_list.append(samples[label][modality])
+    
+    modality_samples = torch.concat(modality_samples_list)
+    samples_cnt = [ms.shape[0] for ms in modality_samples_list]
+
+    reduced_emb = reduction_model.fit_transform(modality_samples)
+    cnt = 0
+    for label in range(num_labels):
+        sc = samples_cnt[label]
+        x = reduced_emb[cnt: cnt+sc+1, 0]
+        y = reduced_emb[cnt: cnt+sc+1, 1]
+        plt.scatter(x, y, label=f"label: {label}")
+        cnt += sc
+    plt.legend()
+    plt.show()
+    return reduced_emb
+
+
+if __name__ == '__main__':
+
+    # load opts
+    with open('config/eval_config.yaml') as f:
+        opts = AttrDict(yaml.safe_load(f))
+
+    # set device
+    device = opts.eval.device
+    if not torch.cuda.is_available():
+        assert 'cuda' not in opts.eval.device, "CUDA is not available."
+
+    # load model
+    model = MMEncoder(opts)
+    model.eval()
+    model.to(device)
+
+    # load ckpt
+    if opts.eval.load_ckpt:
+        ckpt = torch.load(opts.eval.load_ckpt_path, map_location=device)
+        model.load_state_dict(ckpt['model'])
+        print("Checkpoint load complete.")
+
+
+    # dataloader for each class
+    loaders = []
+    for data_path in opts.eval.subset_config_path:
+        with open(data_path) as f:
+            data_opts = AttrDict(yaml.safe_load(f))
+        dataset = MMDataset(data_opts)
+        loader = DataLoader(
+            dataset,
+            batch_size=opts.eval.batch_size,
+        )
+        loaders.append(loader)
+
+    samples = {
+        lbl: {
+            'video': torch.empty(0, opts.model.emb_dim), 
+            'spec': torch.empty(0, opts.model.emb_dim),
+            'audio': torch.empty(0, opts.model.emb_dim)
+        } 
+        for lbl in range(len(loaders))
+    }
+
+
+    prog_bar = tqdm(range(sum([len(loader) for loader in loaders])))
+
+    for lbl_idx, loader in enumerate(loaders):
+
+        # inference and get embeddings
+        for batch in loader:
+            # load batch to device
+            vid_inp = batch['video'].to(device)
+            spec_inp = batch['spec'].to(device)
+            aud_inp = batch['audio'].to(device)
+
+            with torch.no_grad():
+                emb = model(vid_inp, spec_inp, aud_inp)
+                samples[lbl_idx]['video'] = torch.concat([samples[lbl_idx]['video'], emb['video']], dim=0)
+                samples[lbl_idx]['spec'] = torch.concat([samples[lbl_idx]['spec'], emb['spec']], dim=0)
+                samples[lbl_idx]['audio'] = torch.concat([samples[lbl_idx]['audio'], emb['audio']], dim=0)
+            prog_bar.update()
+    reduction_model = TSNE(n_components=2, learning_rate='auto', init='random', perplexity=30.)
+    reduced_emb = analyze_embedding('video', samples, reduction_model)
+
+
